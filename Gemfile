daefcd917021469ab5bbf8a3772a78cc3de7d323
TheLuisAbarca <arclaav.adm@gmail.com>
2022-04-12 11:41:28 -0500

    Add implementation of pagination using kaminari gem for foods and recipes.

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'bootsnap', require: false
 
 gem 'devise'
 
+gem 'kaminari'
+
 # Use Sass to process CSS
 # gem "sassc-rails"
 
--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 
   # GET /recipes or /recipes.json
   def index
-    @recipes = Recipe.all
+    @recipes = Recipe.order(:id).page(params[:page]).per(2)
   end
 
   # GET /recipes/1 or /recipes/1.json
